3bc7c4b5954d078016ee579c0f74bf30849c4e38
Sajithrajan03 <saisajith03@gmail.com>
2023-10-03 05:08:00 +0530

    cs

--- Graph/e.py
+++ Graph/e.py
@@ -1,3 +1,31 @@
-a = {0: [1, 2, 3], 1: [0, 2, 3], 2: [0, 1], 3: [0, 1], 4: []}
-adj_key = [x for x in a.keys()]
-print(adj_key)
+class graph:
+    def __init__(self):
+        self.graph= list
+    def bfs(self,v):
+        visited = set()
+        queue = [v,]
+        while(len(queue)):
+            node = queue.pop(0)
+            if node in visited:
+                pass
+            else:
+                visited.add(node)
+                for i in self.graph[node]:
+                    queue.append(i)
+        print(visited)
+
+    def DFS(self,v):
+        visited = set()
+        self.dfstraversal(visited,v)
+        print(visited)
+    def dfstraversal(self,visited,v):
+        if v in visited:
+            return 
+        visited.add(v)
+        for i in self.graph[v]:
+            self.dfstraversal(visited,i)
+
+g = graph()
+g.graph = {1:[2,3],2:[1,5,6],3:[1,4,7],4:[3,8],5:[2],6:[2],7:[3,8],8:[4,7]}
+g.bfs(1)
+g.DFS(1)
---
+++ Sorting/Algorithm/merge.cpp
@@ -0,0 +1,4 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(int arr[],int )
