cc9a5cd099cebc842d60a102efa5fd29abb524b9
Sajithrajan03 <saisajith03@gmail.com>
2023-10-05 11:06:42 +0530

    CS

--- Sorting/Sorting_Analysis/Work/2.MergeandQuicksort/quick.py
+++ Sorting/Sorting_Analysis/Work/2.MergeandQuicksort/quick.py
@@ -23,17 +23,28 @@ def partition(arr, low, high):
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
     return i + 1
 
-def quick_sort(arr, low, high, threshold=10):
+def quick_sort_wrap(arr, low, high,threshold=10):
     """
     Sort the arr in place using quick sort algorithm. 
     In-place quick sort with Randomized pivot selection
 
-    1. Optimize it with insertion sort for small subarrs and randomize the pivot selection.
+    1. Optimize it with insertion sort for small subarr in our case its 10 and randomize the pivot selection.
     """
+    
+
     if low < high:
         if high - low < threshold:
             insertion_sort(arr, low, high)
+            print("insertion @",low,high)
         else:
+             
             pivot = partition(arr, low, high)
-            quick_sort(arr, low, pivot - 1)
-            quick_sort(arr, pivot + 1, high)
+            quick_sort_wrap(arr, low, pivot - 1)
+            quick_sort_wrap(arr, pivot + 1, high)
+
+length = random.randint(30, 50)
+arr = [random.randint(0, 100) for _ in range(length)]
+
+quick_sort_wrap(arr,0,length-1)
+print(arr)
+
