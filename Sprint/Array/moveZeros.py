2be1e6d9866c3cf3be33164116fa6bb4516c84aa
Sajithrajan03 <saisajith03@gmail.com>
2024-05-03 16:16:13 +0530

    zero

---
+++ Sprint/Array/moveZeros.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        j = -1
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                j = i
+                break
+        if j == -1:
+            return
+        for i in range(j+1,len(nums)):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                nums[i] = 0
+                j += 1
+             
